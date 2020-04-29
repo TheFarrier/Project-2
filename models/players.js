f37dbaf2093e051f59e537e312ba127e61e8ca34
The Farrier <nkfarrey@gmail.com>
2020-04-29 18:55:54 -0500

    ran lint remover

--- models/players.js
+++ models/players.js
@@ -1,28 +1,27 @@
 module.exports = function (sequelize, DataTypes) {
-  var Players = sequelize.define("Players", {
+  const Players = sequelize.define('Players', {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        len: [3]
-      }
+        len: [3],
+      },
     },
     selection: {
       type: DataTypes.STRING,
       validate: {
-        len: [8]
-      }
+        len: [8],
+      },
     },
     score: {
       type: DataTypes.INTEGER,
-      defaultValue: 0
-    }
+      defaultValue: 0,
+    },
   });
 
   Players.associate = function (models) {
-
-    Players.belongsTo(models.Users)
-  }
+    Players.belongsTo(models.Users);
+  };
 
   return Players;
 };
--- models/users.js
+++ models/users.js
@@ -1,23 +1,23 @@
 module.exports = function (sequelize, DataTypes) {
-  var Users = sequelize.define("Users", {
+  const Users = sequelize.define('Users', {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        len: [3]
-      }
+        len: [3],
+      },
     },
     password: {
       type: DataTypes.TEXT,
       allowNull: false,
       validate: {
-        len: [8]
-      }
+        len: [8],
+      },
     },
     gamesWon: {
       type: DataTypes.INTEGER,
-      defaultValue: 0
-    }
+      defaultValue: 0,
+    },
   });
   return Users;
 };
