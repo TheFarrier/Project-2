06a8f1be99c031e2bb7f815ed73c20d98fb30e34
Damian <damianisaacalexander@gmail.com>
2020-05-01 22:07:41 -0500

    passport auth added, bcrypt encrypt moved to be a prototype on user model, express router implemented, routes reoriganized to be located under src folder, logout, register, and signin routes implemented

--- models/users.js
+++ models/users.js
@@ -1,6 +1,8 @@
+const bcrypt = require('bcrypt');
+
 module.exports = function (sequelize, DataTypes) {
-  const Users = sequelize.define('Users', {
-    username: {
+  const User = sequelize.define('User', {
+    email: {
       type: DataTypes.TEXT,
       allowNull: false,
       validate: {
@@ -11,10 +13,14 @@ module.exports = function (sequelize, DataTypes) {
       type: DataTypes.TEXT,
       allowNull: false,
     },
-    gamesWon: {
-      type: DataTypes.INTEGER,
-      defaultValue: 0,
-    },
-  }, { freezeTableName: true });
-  return Users;
+  });
+
+  User.prototype.validatePass = function (password) {
+    return bcrypt.compareSync(password, this.password);
+  };
+  User.addHook('beforeCreate', (user) => {
+    // eslint-disable-next-line no-param-reassign
+    user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10, null));
+  });
+  return User;
 };
