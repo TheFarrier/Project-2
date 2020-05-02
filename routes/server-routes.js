e1b19501b6ce113bb4f6b5a71e4c772073551469
The Farrier <nkfarrey@gmail.com>
2020-05-02 12:53:49 -0500

    moved posts to api routes

--- routes/server-routes.js
+++ routes/server-routes.js
@@ -14,33 +14,12 @@ module.exports = function (app) {
     res.render('login');
   });
 
-  app.post('/login', (req, res) => {
-    const pass = req.body.password;
-
-    db.Users.findAll({ where: { username: req.body.username }, raw: true }).then((res) => {
-      console.log(res[0].password);
-      bcrypt.compare(pass, res[0].password, (err, response) => {
-        if (response === true) {
-          // route to game page
-        } else if (response !== true) {
-          res.render('login');
-        }
-      });
-    });
-  });
-
   app.get('/register', (req, res) => {
     res.render('register');
   });
 
-  app.post('/register', (req, res) => {
-    bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
-      db.Users.create({
-        username: req.body.username,
-        password: hash,
-      });
-    });
-    // redirect to next page I.E 'Lobby'
+  app.get('/vote', (req, res) => {
+    res.render('vote');
   });
 };
 // eslint-disable-next-line no-unused-expressions
