84aa273633e4fd54bb730f66bbdc676b77d07f26
Damian <damianisaacalexander@gmail.com>
2020-05-06 16:57:46 -0500

    updated socket

--- public/scripts.js
+++ public/scripts.js
@@ -4,9 +4,37 @@ const socket = io('http://localhost:3000', { timeout: 2000 });
 socket.on('playerListener', (players) => {
     console.log(players);
 });
+socket.on('stateListener', (data) => {
+    $('#gameWindow').empty();
+    $('#gameWindow').append(data.html);
+    $('.ready-up').css('display', 'none')
+    $('.ready-up').css('opacity', '0')
+    $('.announcer-text').css('opacity', '0')
+    $('.announcer-gif').css('opacity', '0')
+
+    $('.announcer-text').animate({ transition: '.1s', opacity: 1 });
+    $('.announcer-gif').animate({ transition: '.1s', opacity: 1 });
+    $('.announcer-text').text('Hold on a sec... rounding up players!');
+    $('.announcer-gif').attr('src', ' https://media.giphy.com/media/LOZ5ELmGUUiC4/giphy.gif');
+    $('.announcer-gif').attr('alt', 'gathering');
+    })
 socket.on('redirect', (data) => {
-//  socket.disconnect();
- // window.location.href = data.url;
+  socket.disconnect();
+  window.location.href = data.url;
+});
+
+socket.on('playersLoaded', () => {
+    console.log('players loaded')
+    $('.ready-up').css('display', 'block')
+    $('.ready-up').animate({transition: '2s', opacity: 1})
+    $('.ready-up').one('click', (e)=> {
+        socket.emit('playerReady', {ready: true});
+        searchState();
+    });
 });
 
 
+//    $('.announcer-text').animate({ transition: '.3s', opacity: 0 });
+//$('.announcer-gif').animate({ transition: '.3s', opacity: 0 });
+//$('.ready-up').animate({ transition: '.0s', opacity: 0 });
+//socket.emit('playerReady', {ready: true});
--- sockets/mainSocket.js
+++ sockets/mainSocket.js
@@ -1,24 +1,82 @@
 const handlebars = require('handlebars');
+const gameQuestions = require('./gameQuestions.js');
 const fs = require('fs');
+   
+const readyHTML = fs.readFileSync("./views/game-partials/ready.handlebars", "utf8")
+const searchHTML = fs.readFileSync("./views/game-partials/search.handlebars", "utf8")
 
 function mainSocket(io) {
-let players = [];
+  let players = [];
+  let readyCheck = [];
+  let questionHistory = [];
+  let maxPlayers = 2;
+  let gameTime = 300;
 
-io.on('connection', (socket) => {
+  io.on('connection', (socket) => {
+
     if (socket.request.session.passport) {
       let playerTag = socket.request.session.passport.user;
-      if(players.indexOf(playerTag) === -1){
-       players.push(playerTag);
-      }
-      socket.join('lobby', () => {
-        io.to('lobby').emit('playerListener', players);
+      console.log(`${playerTag} has connected`)
+      io.clients((err, clients) => {
+        if (clients.length <= maxPlayers){
+          if(players.indexOf(playerTag) === -1) {
+            players.push(playerTag);
+          }
+          socket.join('lobby', () => {
+            console.log(`${playerTag} has joined lobby`)
+            io.to('lobby').emit('playerListener', {playersList: players});
+            io.to('lobby').emit('stateListener', {html: readyHTML});
+            if(players.length === maxPlayers){
+              io.of('/').in('lobby').emit('playersLoaded');
+            }
+          });
+        }
       });
+
     }
     else {
       socket.emit('redirect', { url: 'http://localhost:3000/auth/logout' });
     }
+    
+    function countDown()
+    {
+      gameTime -= 1;
+    }
+    socket.on('gifSelected', () => {
+      console.log(socket.request.session.passport.user)
+    })
+    socket.on('playerReady', (data) => {
+      readyCheck.push(data.ready);
+      if(readyCheck.length === maxPlayers){
+       let questionText = generateQuestion();
+       io.of('/').in('lobby').emit('startGame', {question: questionText, html: searchHTML});
+        let gameTimer = setInterval(countDown, 1000);
+      }
+    });
+
   });
+ 
+  function generateQuestion()
+  {
+    console.log(gameQuestions.length);
+    let generatedIndex = Math.floor(Math.random() * gameQuestions.length)
+    let generatedQuestion = gameQuestions[generatedIndex];
+    if(questionHistory.indexOf(generatedQuestion) === -1){
+      questionHistory.push(generatedQuestion)
+      return generatedQuestion.question;
+    }
+    else if(questionHistory.length != gameQuestions.length){
+      generateQuestion();
+    } 
+    else
+    {
+      return 'Out of questions!'
+    }
+  }
 }
+
+
+
 
 module.exports = mainSocket;
 
